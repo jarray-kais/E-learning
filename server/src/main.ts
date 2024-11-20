6d9d886c72b907222bd33eeabf4ac436a948a5e2
jarray-kais <jarraykais1@gmail.com>
2024-11-21 00:08:51 +0100

    update
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.init();
-
-  // Export the server for Vercel
-  return server;
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
-
-bootstrap().then((server) => {
-  module.exports = server;
-});
+bootstrap();
